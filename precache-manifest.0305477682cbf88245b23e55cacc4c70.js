85ab868404df666fc58aabba0b4301e744171800
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2021-07-10 15:13:02 +0530

    Updates

---
+++ precache-manifest.0305477682cbf88245b23e55cacc4c70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5275a36432db5f1896c62ad30357f27",
+    "url": "/whats-app/index.html"
+  },
+  {
+    "revision": "1da62f65630e1b075edb",
+    "url": "/whats-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bcaa9aa96f0df5fec807",
+    "url": "/whats-app/static/css/main.7595bc90.chunk.css"
+  },
+  {
+    "revision": "1da62f65630e1b075edb",
+    "url": "/whats-app/static/js/2.3bead84e.chunk.js"
+  },
+  {
+    "revision": "95e5393dbc94b774bee63119a26602ef",
+    "url": "/whats-app/static/js/2.3bead84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcaa9aa96f0df5fec807",
+    "url": "/whats-app/static/js/main.23f4916e.chunk.js"
+  },
+  {
+    "revision": "b27e15788c11512b45f8",
+    "url": "/whats-app/static/js/runtime-main.42253129.js"
+  }
+]);
